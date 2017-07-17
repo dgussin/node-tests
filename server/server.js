08aca6f0c115bc255253b7357a689286929e198e
Darren Gussin <dgussin@msn.com>
2017-07-16 22:44:09 -0400

    test express app

--- server/server.js
+++ server/server.js
@@ -3,8 +3,30 @@ const express = require('express');
 var app = express();
 
 app.get('/', (req, res) =>{
-    res.send('Hello world!');
+    res.status(404).send({
+        error: 'Page not found.',
+        name: 'Todo App v1.0'
+    });
 });
+
+// GET /users
+// give users a name prop and age prop
+app.get('/users', (req, res) => {
+    res.status(200).send([{
+        name: 'Darren Gussin',
+        age: 48
+    },
+    {
+        name: 'Jennifer Gussin',
+        age: 43
+    },
+    {
+        name: 'Diane Gussin',
+        age: 76
+    }]);
+});
+
 
 app.listen(3000);
 
+module.exports.app = app;
--- video.md
+++ video.md
@@ -6,4 +6,5 @@
 * 52 - Watch and Auto Restart Tests
 * 53 - Using an Assertion Library
 * 54 - Testing Asynchronous Code
-* 55 - Testing Express Application: Part 1
+* 55 - Testing Express Applications: Part 1
+* 56 - Testing Express Applications: Part 2
