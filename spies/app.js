400260f49207408f36949d521ea2a2bcd4521ac0
Darren Gussin <dgussin.shiftdigital@outlook.com>
2017-07-24 09:26:20 -0400

    final change

--- spies/app.js
+++ spies/app.js
@@ -1,9 +1,8 @@
+var db = require('./db.js');
+
 
 module.exports.handleSignup = (email, password) => {
   // check if the email already exists
-  db.saveUser({
-    email,
-    password
-  })
+  db.saveUser({email,password});
   // send the welcome email
 }
